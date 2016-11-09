23940ab7d6a9d794474786f879edef8e90266cf8
joaobcampos <joaobcampos@hotmail.com>
2016-11-09 09:29:51 +0100

    Update Cobject.h

--- Cobject.h
+++ Cobject.h
@@ -1,11 +1,30 @@
 #ifndef COBJECT_H
 #define COBJECT_H
 
+class GLfloat;
+
 class Cobject{
 	public:
-		Cobject(GLuint programID, std::string shaders) : binder(programID), shaders(shaders){};
+		Cobject(GLuint vb, GLuint cb, const glm::mat4 &proj, const glm::mat4 &vw_, const glm::mat4 &mdl) : vertexbuffer(vb), colorbuffer(cb), Projection(proj), View(vw), Model(mdl){};
 		~Cobject();
 	
+		void set_projection(const glm::mat4 &proj){
+			Projection = proj;
+		}
+	
+		void set_view(const glm::mat4 &vw_){
+			View = vw;
+		}
+	
+		void set_model(const glm::mat4 &mdl){
+			Model = &mdl;
+		}	
+		
+		GLuint get_vertexbuffer();
+		GLuint get_colorbuffer();
+		void bind_vertex_buffer(GLfloat data_buffer[]);
+		void bind_vertex_buffer(GLfloat data_buffer[]);
+		glm::mat4 get_mvp();
 		
 	private:
 		// Representation of the outside perspective
@@ -15,6 +34,7 @@ class Cobject{
 		// Intrinsic information of the object (position and color)
 		GLuint vertexbuffer;
 	 	GLuint colorbuffer;
+		void bind_buffer(GLuint &buf, GLfloat data_buffer[]);
 }
 
 #endif;

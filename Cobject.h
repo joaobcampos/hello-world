b19eae858301425482f08d1a347b2e4eef3c9fa6
joaobcampos <joaobcampos@hotmail.com>
2016-11-09 23:33:21 +0100

    Update Cobject.h
    Changed class declaration. The ultimate purpose is to make each object contain the information to draw itself

--- Cobject.h
+++ Cobject.h
@@ -1,30 +1,35 @@
 #ifndef COBJECT_H
 #define COBJECT_H
 
-class GLfloat;
+
+#include <GL/glew.h>
+#include <glm/glm.hpp>
 
 class Cobject{
 	public:
-		Cobject(GLuint vb, GLuint cb, const glm::mat4 &proj, const glm::mat4 &vw_, const glm::mat4 &mdl) : vertexbuffer(vb), colorbuffer(cb), Projection(proj), View(vw), Model(mdl){};
+		Cobject(glm::mat4 proj, glm::mat4 vw, glm::mat4 mdl,
+			   	GLuint vb, GLuint cb,
+				GLfloat g_v_buffer_data[], int v_buffer_size,
+				GLfloat g_c_buffer_data[], int c_buffer_size
+			   );
 		~Cobject();
 	
-		void set_projection(const glm::mat4 &proj){
-			Projection = proj;
-		}
+		void set_projection(const glm::mat4 &proj);
 	
-		void set_view(const glm::mat4 &vw_){
-			View = vw;
-		}
+		void set_view(const glm::mat4 &vw);
 	
-		void set_model(const glm::mat4 &mdl){
-			Model = &mdl;
-		}	
+		void set_model(const glm::mat4 &mdl);
 		
 		GLuint get_vertexbuffer();
 		GLuint get_colorbuffer();
 		void bind_vertex_buffer(GLfloat data_buffer[]);
-		void bind_vertex_buffer(GLfloat data_buffer[]);
+		void bind_color_buffer(GLfloat data_buffer[]);
 		glm::mat4 get_mvp();
+		void enable_vertex_buffer();
+		void enable_color_buffer();
+		void disable_vertex_buffer();
+		void disable_color_buffer();
+		void draw(int n);
 		
 	private:
 		// Representation of the outside perspective
@@ -34,7 +39,14 @@ class Cobject{
 		// Intrinsic information of the object (position and color)
 		GLuint vertexbuffer;
 	 	GLuint colorbuffer;
+		
+		GLfloat * g_vertex_buffer_data;
+		GLfloat * g_color_buffer_data;
+	
+		int vertex_buffer_size;
+		int color_buffer_size;
+		
 		void bind_buffer(GLuint &buf, GLfloat data_buffer[]);
-}
+};
 
-#endif;
+#endif

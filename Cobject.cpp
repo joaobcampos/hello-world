7e9864a7d29d80f8f2a2bdc27e604cdeefd82e2e
joaobcampos <joaobcampos@hotmail.com>
2016-11-12 21:21:58 +0100

    Update Cobject.cpp
    The cube is moving. Perhaps not in the most efficient way but it is. VertexArrayID was the cause it was not appearing.

--- Cobject.cpp
+++ Cobject.cpp
@@ -1,30 +1,34 @@
 #include "Cobject.h"
 #include <cstdlib>
 #include <iostream>
+#include <random>
 
 Cobject::Cobject(glm::mat4 proj, glm::mat4 vw, glm::mat4 mdl,
 				GLfloat g_v_buffer_data[], 
-				GLfloat g_c_buffer_data[] 
+				GLfloat g_c_buffer_data[], int buf_size 
 			   ){
 
 	Projection = proj;
 	View = vw;
 	Model = mdl;
 	
+	VertexArrayID = new GLuint;
+	glGenVertexArrays(1, VertexArrayID);
+	glBindVertexArray(*VertexArrayID);
+	
+	
 	buffer = new GLuint(2);
-
+	
 	glGenBuffers(2, buffer);
 	glBindBuffer(GL_ARRAY_BUFFER, buffer[0]);
-	glBufferData(GL_ARRAY_BUFFER, sizeof(g_v_buffer_data), g_v_buffer_data, GL_STATIC_DRAW);
-
-	GLuint colorbuffer;
+	glBufferData(GL_ARRAY_BUFFER, buf_size * sizeof(g_v_buffer_data[0]), g_v_buffer_data, GL_STATIC_DRAW);
 	
 	glBindBuffer(GL_ARRAY_BUFFER, buffer[1]);
-	glBufferData(GL_ARRAY_BUFFER, sizeof(g_c_buffer_data), g_c_buffer_data, GL_STATIC_DRAW);
+	glBufferData(GL_ARRAY_BUFFER, buf_size * sizeof(g_c_buffer_data[0]), g_c_buffer_data, GL_STATIC_DRAW);
 }
 
 Cobject::~Cobject(){
-	glDeleteBuffers(2, buffer);
+	//glDeleteBuffers(2, buffer);
 }
 
 void Cobject::set_projection(const glm::mat4 &proj){
@@ -44,12 +48,17 @@ glm::mat4 Cobject::get_mvp(){
 	return Projection * View * Model;
 }
 
+void Cobject::bind_new_vertex_buffer(GLfloat g_new_v_buffer[], int buf_size){
+	glBindBuffer(GL_ARRAY_BUFFER, buffer[0]);
+	glBufferData(GL_ARRAY_BUFFER, buf_size * sizeof(g_new_v_buffer[0]), g_new_v_buffer, GL_STATIC_DRAW);
+}
 
 void Cobject::enable_buffers(){
 	if (buffer != NULL){
 		glEnableVertexAttribArray(0);
 		glBindBuffer(GL_ARRAY_BUFFER, buffer[0]);
 		glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 0, (void*)0);
+		
 		glEnableVertexAttribArray(1);
 		glBindBuffer(GL_ARRAY_BUFFER, buffer[1]);
 		glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 0, (void*)0);
@@ -63,8 +72,14 @@ void Cobject::disable_buffers(){
 
 void Cobject::delete_buffers(){
 	glDeleteBuffers(2, buffer);
+	glDeleteVertexArrays(1, VertexArrayID);
+
 }
 
 void Cobject::draw(int n){
 	glDrawArrays(GL_TRIANGLES, 0, n*3); 
+}
+
+GLuint * Cobject::get_buffer(){
+	return buffer;
 }
